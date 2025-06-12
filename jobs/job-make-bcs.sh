ce91bb6b25ec82bd47b62e9ac880791a13068e48
Jun.Wang <Jun.Wang@noaa.gov>
2025-06-12 19:45:53 +0000

    use atparse to allow us differnt job account

--- jobs/job-make-bcs.sh
+++ jobs/job-make-bcs.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=make_bcs
 #SBATCH --output=logs/make_bcs_%j.out
 #SBATCH --partition=u1-compute
-#SBATCH --account=gsd-hpcs
+#SBATCH --account=@[ACCNR]
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=16
@@ -11,12 +11,13 @@
 # conda
 source etc/env.sh
 
-year=${1:-2024}
-month=${2:-05}
-day=${3:-06}
-hour=${4:-23}
-lead_hour=${5:-18}
-init_time="${year} ${month} ${day} ${hour}"
+# set vars
+init_time="@[INIT_TIME]"
+lead_hour=@[LEAD_HOUR]
+year=`echo $init_time |cut -c1-4`
+month=`echo $init_time |cut -c6-7`
+day=`echo $init_time |cut -c9-10`
+hour=`echo $init_time |cut -c12-13`
 date_str="${year}${month}${day}_${hour}"
 
 python3 src/make_bcs.py net-diffusion/normalize.nc ${init_time} ${lead_hour} --hrrr_grid_file "${date_str}/hrrr_${date_str}_surface.grib2"
