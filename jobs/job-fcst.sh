a80309da1ae9660b3a6d0a83966a9e022241622d
Daniel Abdi <daniel.abdi@noaa.gov>
2026-03-04 17:04:35 -0700

    Set minimum memory for forecast job

--- jobs/job-fcst.sh
+++ jobs/job-fcst.sh
@@ -9,6 +9,7 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=96
 #SBATCH --time=@[FCST_WALLTIME]
+#SBATCH --mem=128G
 
 # load wgrib2 modules
 module use /contrib/spack-stack/spack-stack-1.9.1/envs/ue-oneapi-2024.2.1/install/modulefiles/Core/
